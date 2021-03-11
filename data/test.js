acad939cdfd770cd15a40e34eff3a8e0c7b72860
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2021-03-11 16:36:14 +0800

    leaflet stuff

---
+++ data/test.js
@@ -0,0 +1,71 @@
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            122.5909423828125,
+            11.43291712063836
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            122.67608642578126,
+            11.517705009897712
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            122.72415161132812,
+            11.447723189292294
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            122.74612426757812,
+            11.590360006513054
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            122.772216796875,
+            11.561771167937778
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            122.7256965637207,
+            11.547139279947313
+          ]
+        }
+      }
+    ]
+  }
